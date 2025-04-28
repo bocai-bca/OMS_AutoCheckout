5743e6259fbe7c1ae2ccd7dd65986bed3c32a306
bocai-bca <105545253+bocai-bca@users.noreply.github.com>
2025-04-29 01:45:23 +0800

    Upload

---
+++ AutoCheckout.cs
@@ -0,0 +1,68 @@
+﻿using BepInEx;
+using HarmonyLib;
+using BepInEx.Logging;
+using BepInEx.Unity.Mono;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.CompilerServices;
+
+namespace OMS_AutoCheckout;
+
+[BepInPlugin("oms_autocheckout", "AutoCheckout", "0.0.0")]
+public class AutoCheckout : BaseUnityPlugin
+{
+	internal static new ManualLogSource Logger;
+
+	private void Awake()
+	{
+		Logger = base.Logger;
+		Logger.LogInfo($"Plugin oms_autocheckout is loaded!");
+		Harmony harmony = new("oms_autocheckout");
+		harmony.PatchAll();
+	}
+	[HarmonyPatch(typeof(UIManager), "OpenJournal")]
+	public static class Injector
+	{
+		public static void Postfix()
+		{
+			if (IsInjected)
+			{
+				return;
+			}
+			GameObject gameObject = new();
+			gameObject.AddComponent<Loop>();
+			Object.DontDestroyOnLoad(gameObject);
+			IsInjected = true;
+		}
+	}
+	static bool IsInjected = false;
+	static Checkout[] CheckoutList = [];
+	static int SearchTimer = 120;
+	public class Loop : MonoBehaviour
+	{
+		private void Update()
+		{
+			SearchTimer--;
+			if (SearchTimer <= 0)
+			{
+				CheckoutList = Object.FindObjectsByType<Checkout>(FindObjectsSortMode.None);
+				SearchTimer = 120;
+			}
+			foreach (Checkout checkout in CheckoutList)
+			{
+				if (!checkout.IsHost)
+				{
+					return;
+				}
+				List<CheckoutItem> checkoutItems = checkout.GetCheckoutItems();
+				if (checkoutItems.Count > 0)
+				{
+					checkoutItems[0].EmployeeInteract();
+					return;
+				}
+				CoinPouch coinPouch = checkout.GetCoinPouch();
+				coinPouch?.EmployeeInteract();
+			}
+		}
+	}
+}
